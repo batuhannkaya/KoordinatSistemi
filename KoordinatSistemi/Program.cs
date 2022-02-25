d1a8d1e1e702c4a7ba1daf2976fdbaf80033f4c0
BATUHAN <BATUHAN@DESKTOP-9MKVHE2>
2022-02-25 10:58:14 +0300

    Proje dosyası ekle.

---
+++ KoordinatSistemi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace KoordinatSistemi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = -5; i <= 5; i++)
+            {
+                for (int j = -5; j <= 5; j++)
+                {
+                    if (i >= 0)
+                    {
+                        Console.Write($" ( {i},{j}) ");
+                    }
+                    else
+                    {
+                        Console.Write($" ({i},{j}) ");
+                    }
+                }
+                Console.WriteLine(); Console.WriteLine();
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
